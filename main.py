0622ec0d84c24af765b63a8520ad6fc8b5181b85
Tako <takosabu94@gmail.com>
2026-03-05 19:33:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()
-token = os.getenv("TOKEN")
+tokens = {
+    "idle": os.getenv("TOKEN1"),
+    "online": os.getenv("TOKEN2")
+}
 
 app = Flask('')
 @app.route('/')
@@ -18,35 +21,17 @@ def run_flask():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    from threading import Thread
-    t = Thread(target=run_flask)
+    t = threading.Thread(target=run_flask)
     t.daemon = True
     t.start()
 
 def send_heartbeat(ws, interval):
     while True:
         time.sleep(interval)
-        ws.send(json.dumps({"op": 1, "d": None}))
-
-def on_open(ws):
-    auth = {
-        "op": 2,
-        "d": {
-            "token": token,
-            "properties": {
-                "$os": "Linux",
-                "$browser": "Discord Client",
-                "$release_channel": "stable",
-                "$client_build_number": 117300,
-                "$client_event_source": None
-            },
-            "presence": {
-                "status": "idle",
-                "afk": False
-            }
-        }
-    }
-    ws.send(json.dumps(auth))
+        try:
+            ws.send(json.dumps({"op": 1, "d": None}))
+        except:
+            break
 
 def on_message(ws, message):
     data = json.loads(message)
@@ -54,12 +39,41 @@ def on_message(ws, message):
         interval = data['d']['heartbeat_interval'] / 1000
         threading.Thread(target=send_heartbeat, args=(ws, interval), daemon=True).start()
     if data.get('t') == "READY":
-        print(f"Logged in as {data['d']['user']['username']}")
+        print(f"Logged in: {data['d']['user']['username']}")
 
-ws = websocket.WebSocketApp("wss://gateway.discord.gg/?v=9&encoding=json",
-                            on_open=on_open,
-                            on_message=on_message)
+def create_ws(token, status):
+    def on_open(ws):
+        auth = {
+            "op": 2,
+            "d": {
+                "token": token,
+                "properties": {
+                    "$os": "Linux",
+                    "$browser": "Discord Client",
+                    "$device": "discord.py"
+                },
+                "presence": {
+                    "status": status,
+                    "afk": False
+                }
+            }
+        }
+        ws.send(json.dumps(auth))
+
+    ws = websocket.WebSocketApp("wss://gateway.discord.gg/?v=9&encoding=json",
+                                on_open=on_open,
+                                on_message=on_message)
+    ws.run_forever()
 
 if __name__ == "__main__":
     keep_alive()
-    ws.run_forever()
+
+    t1 = threading.Thread(target=create_ws, args=(tokens["idle"], "idle"))
+
+    t2 = threading.Thread(target=create_ws, args=(tokens["online"], "online"))
+    
+    t1.start()
+    t2.start()
+    
+    t1.join()
+    t2.join()
